23e09b8c2189abc03dd8a347dc1222b39d573ada
quan831993 <quan831993@gmail.com>
2023-03-15 08:27:43 +0700

    feature/#761/checkout

--- bookland/src/App.js
+++ bookland/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes, Link } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Checkout from './components/Checkout';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<Homepage/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/signup' element={<Signup/>}/>
+      <Route path='/checkout' element={<Checkout/>}/>
     </Routes>
   );
 }
